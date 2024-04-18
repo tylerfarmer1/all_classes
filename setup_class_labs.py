16f736836cb5ca412b94dca1e6b09d121af0c7bc
Tyler Farmer <72703514+tylerfarmer1@users.noreply.github.com>
2024-04-18 13:23:39 -0700

    Add files via upload

---
+++ setup_class_labs.py
@@ -0,0 +1,96 @@
+import os, sys
+import requests
+import zipfile
+
+base_url = "https://github.com/tylerfarmer1/all_classes/raw/main/"
+lab_folders = ["AI-050", "DP-203", "PL-100", "PL-200", "PL-500"]
+disk_drive = r"E:\\"
+
+# Function to ask a user which lab then want
+def make_choice():
+    # Print the list with item numbers
+    print("Select an option:")
+    for index, folder in enumerate(lab_folders, 1):
+        print(f"{index}: {folder}")
+    
+    # Ask for user input
+    choice = input("Enter the number corresponding to your choice: ")
+    
+    # Check if the choice is valid
+    if choice.isdigit() and 1 <= int(choice) <= len(lab_folders):
+        return(lab_folders[int(choice) - 1])
+    else:
+        print("No option selected, program terminated.")
+        sys.exit()
+
+# function to delete the existing folder
+def delete_existing(choice):
+    print("\nStarting Deletion Process:")
+    folder_path = disk_drive + choice
+    
+    if os.path.exists(folder_path):
+        for root,dirs,files in os.walk(folder_path, topdown=False):
+            for name in files:
+                try:
+                    os.remove(os.path.join(root,name))
+                except Exception as e:
+                    print(f"This file failed to delete: {name} but continuing on.")
+            for name in dirs:
+                try:
+                    os.rmdir(os.path.join(root, name))
+                except Exception as e:
+                    print(f"This folder failed to delete: {name} but continuing on.")
+        try:
+            os.remove(folder_path)
+        except Exception as e:
+            print(f"Failed to delete root folder {folder_path} but continuing on.")
+    else:
+        print(f"Folder {folder_path} does not exist.  Continuing on.")
+
+
+# Function to download and extract the zip file
+def download_and_extract(choice):
+
+    print("\nStarting Download Process:")
+    my_zip_file = choice + ".zip"
+    url = base_url + my_zip_file
+    
+    print(f"Downloading file {my_zip_file} from URL {url}")
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        # Save the zip file in the root of the disk:
+        with open(os.path.join(disk_drive, my_zip_file), "wb") as f:
+            f.write(response.content)
+    else:
+        print("Failed to download the file.  Exiting Program.")
+        sys.exit()
+
+    
+    print("\nStarting Extraction Process:")
+    extract_path = disk_drive + choice
+    print(f"Extracting to folder {extract_path}")
+    full_file_name = disk_drive + my_zip_file
+
+    with zipfile.ZipFile(full_file_name, "r") as zip_ref:
+        for file in zip_ref.namelist():
+            try:
+                zip_ref.extract(file, extract_path)
+            except PermissionError as e:
+                print(f"Permission error with {file}, skipping that file and continuing on.")
+            except Exception as e:
+                print(f"Error extracting {file} with error {e} but continuing on.")
+    
+    # Delete the Zip File
+    os.remove(full_file_name)
+
+
+my_choice=make_choice()
+delete_existing(my_choice)
+download_and_extract(my_choice)
+
+
+
+
+
+
