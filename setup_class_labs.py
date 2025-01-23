d5a56717e513ac5dc1e220b2137cded82726eb11
Tyler Farmer <72703514+tylerfarmer1@users.noreply.github.com>
2025-01-23 05:31:20 -0700

    Update setup_class_labs.py

--- setup_class_labs.py
+++ setup_class_labs.py
@@ -4,7 +4,7 @@ import zipfile
 
 base_url = "https://github.com/tylerfarmer1/all_classes/raw/main/"
 #lab_folders = ["AI-050", "DP-900", "DP-100", "DP-203", "DP-300", "PL-900", "PL-100", "PL-200", "PL-400", "PL-500", "PL-7000"]
-lab_folders = ["AI-050", "PL-200", "PL-400", "PL-500", "PL-7001","PL-7002","PL-7003", "PL-7008", "PL-900"]
+lab_folders = ["AI-050", "PL-200", "PL-400", "PL-500", "PL-7001","PL-7002","PL-7003", "PL-7004", "PL-7008", "PL-900"]
 #lab_folders.sort()
 disk_drive = r"D:\\"
 
