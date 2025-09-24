2310ea6f36acc3e708716f6f73a04e7d118e8f9e
Tyler Farmer <72703514+tylerfarmer1@users.noreply.github.com>
2025-09-24 04:57:28 -0700

    Update setup_class_labs.py

--- setup_class_labs.py
+++ setup_class_labs.py
@@ -6,29 +6,24 @@ base_url = "https://github.com/tylerfarmer1/all_classes/raw/main/"
 #lab_folders = ["AI-050", "DP-900", "DP-100", "DP-203", "DP-300", "PL-900", "PL-100", "PL-200", "PL-400", "PL-500", "PL-7000"]
 lab_folders = ["AI-900", "AI-102", "MS-4018", "PL-200", "PL-400", "PL-500", "PL-7001","PL-7002","PL-7003", "PL-7004", "PL-7008", "PL-900"]
 #lab_folders.sort()
-disk_drive = r"D:\\"
+disk_drive = r"C:\\test\\"
 
 # Function to ask a user which lab then want
 def make_choice():
-    # Print the list with item numbers
-    # print("\n\nI am Here")
-    print("\n\n\nHere is a list of classes. Please enter the class number to retrieve: ")
-    for index, folder in enumerate(lab_folders, 1):
-        print(f"{index}: {folder}")
-    
     # Ask for user input
-    choice = input("Enter the number corresponding to your class: ")
-    
-    # Check if the choice is valid
-    if choice.isdigit() and 1 <= int(choice) <= len(lab_folders):
-        return(lab_folders[int(choice) - 1])
+    choice = input("Type in the course number such as AI-900, PL-900, PL-7000, MS-4018, et cetera: ").strip().upper()
+
+    print("You selected: " + choice)
+
+    if len(choice)>5:
+        return(choice)
     else:
         print("No option selected, program terminated.")
         sys.exit()
 
 # function to delete the existing folder
 def delete_existing(choice):
-    #print("\nStarting Deletion Process:")
+    print("\nStarting Deletion Process:")
     folder_path = disk_drive + choice
     
     if os.path.exists(folder_path):
@@ -37,21 +32,21 @@ def delete_existing(choice):
                 try:
                     os.remove(os.path.join(root,name))
                 except Exception as e:
-                    #print(f"This file failed to delete: {name} but continuing on.")
+                    #print(f"AA This file failed to delete: {name} but continuing on.")
                     print("Continuing...")
             for name in dirs:
                 try:
                     os.rmdir(os.path.join(root, name))
                 except Exception as e:
-                    #print(f"This folder failed to delete: {name} but continuing on.")
+                    #print(f"BB This folder failed to delete: {name} but continuing on.")
                     print("Continuing...")
         try:
             os.remove(folder_path)
         except Exception as e:
-            #print(f"Failed to delete root folder {folder_path} but continuing on.")
+            #print(f"CC Failed to delete root folder {folder_path} but continuing on.")
             print("Continuing...")
     else:
-        #print(f"Folder {folder_path} does not exist.  Continuing on.")
+        #print(f"DD Folder {folder_path} does not exist.  Continuing on.")
         print("Continuing...")
 
 
@@ -70,7 +65,7 @@ def download_and_extract(choice):
         with open(os.path.join(disk_drive, my_zip_file), "wb") as f:
             f.write(response.content)
     else:
-        print("Failed to download the file.  Exiting Program.")
+        print("Failed to download the file. Please check the course number you entered and try again. Exiting Program.")
         sys.exit()
 
     
